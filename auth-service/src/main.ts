594cd03a37cb6cf6bf5b2502ca802c148d6057a9
YohanesCP <yohaneschristianp176@gmail.com>
2026-05-30 11:34:27 +0700

    add auth

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+
+  //pipe line
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Jomoro Koffee - Auth Service')
+    .setDescription('API dokumentasi untuk autentikasi user')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  // 4. Jalankan di port 3001 sesuai ketentuan
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  console.log(`Auth Service is running on: http://localhost:${port}`);
+  console.log(`Swagger Documentation: http://localhost:${port}/api/docs`);
 }
 bootstrap();
